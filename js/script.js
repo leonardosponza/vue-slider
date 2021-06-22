062cb80b20654f01542f7b33ad019a02f0d7f481
leonardo <sponzal4@gmail.com>
2021-06-22 17:17:52 +0200

    change

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ const app = new vue(
         data: {
             counter: 0,
             photos : [
-                'https://i.pinimg.com/originals/15/bb/40/15bb40fd839ffb44b03450bac891ec52.jpg',
-                'https://www.diregiovani.it/wp-content/uploads/2019/09/gatto-manggo-8.jpg',
-                'https://metronews.it/sites/default/files/articolo/2019/01/17/animali_foche.jpg',
-                'https://www.viaggiatorinelmondo.com/wp-content/uploads/2015/10/foca0.jpg'
+                'img/img1.jpg',
+                'img/img2.jpg',
+                'img/img3.jpg',
+                'img/img4.jpg'
             ]
         },
         methods:{
